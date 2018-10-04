1c691e0405b024fd840d8363efc1822ae9f02c74
cesar augusto granelli <cesargranelli@gmail.com>
2018-10-04 18:32:44 -0300

    Home-04/10/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnR6dfQA0B5fFsy1EOzO6MNXHQz6gvQgM',
+  authDomain: 'flag-api.firebaseapp.com',
+  databaseURL: 'https://flag-api.firebaseio.com',
+  projectId: 'flag-api',
+  storageBucket: 'flag-api.appspot.com',
+  messagingSenderId: '443896829935'
+};
 
 @NgModule({
   declarations: [
@@ -18,6 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +42,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public confrontos: Observable<any>;/*
+    {
+      'data': '2018-10-14',
+      'hora': '08:30',
+      'rodada': 36,
+      'mandante': {
+        'nome': 'TIGERS',
+        'escudo': 'assets/imgs/logos/sptigers-128x128.png'
+      },
+      'visitante': {
+        'nome': 'GUARÁ',
+        'escudo': 'assets/imgs/logos/whitecranes-128x116.png'
+      }
+    },
+    {
+      'data': '2018-10-14',
+      'hora': '10:00',
+      'rodada': 36,
+      'mandante': {
+        'nome': 'JUNDIAÍ',
+        'escudo': 'assets/imgs/logos/jundiaifootball-128x128.png'
+      },
+      'visitante': {
+        'nome': 'ATIBAIA',
+        'escudo': 'assets/imgs/logos/atibaiasuperchargers-128x128.png'
+      }
+    }
+  ];*/
 
+  constructor(
+    public navCtrl: NavController,
+    public db: AngularFirestore
+  ) {
+    //this.confrontos = db.collection('confrontos').valueChanges();
   }
 
 }
